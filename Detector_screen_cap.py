ca84077a94ffb2e869b9410e8293f5a99d186dd5
Ron King <ronk62@gmail.com>
2023-04-16 18:34:29 -0700

    tensorflow obj detection - ref TheCodingBug

---
+++ Detector_screen_cap.py
@@ -0,0 +1,166 @@
+# no she-bang in Windows
+
+# ref https://www.youtube.com/watch?v=2yQqg_mXuPQ&t=654s
+# initial setup 4/9/2023
+
+import cv2, time, os, tensorflow as tf
+import numpy as np
+from tensorflow.python.keras.utils.data_utils import get_file
+from PIL import ImageGrab, Image, ImageDraw
+
+np.random.seed(123)
+
+class Detector:
+    def __init__(self):
+        pass
+
+    def readClasses(self, classesFilePath):
+        with open(classesFilePath, 'r') as f:
+            self.classesList = f.read().splitlines()
+
+        # Colors list
+        self.colorList = np.random.uniform(low=0, high=255, size=(len(self.classesList), 3))
+
+        print(len(self.classesList), len(self.colorList))
+
+    def downloadModel(self, modelURL):
+
+        fileName = os.path.basename(modelURL)
+        self.modelName = fileName[:fileName.index('.')]
+
+        ## for testing/debugging
+        # print(fileName)
+        # print(self.modelName)
+
+        self.cacheDir = "./pretrained_models"
+
+        os.makedirs(self.cacheDir, exist_ok=True)
+
+        get_file(fname=fileName, origin=modelURL, cache_dir=self.cacheDir, 
+                 cache_subdir="checkpoints", extract=True)
+    
+    def loadModel(self):
+        print("Loading Model " + self.modelName)
+        tf.keras.backend.clear_session()
+        self.model = tf.saved_model.load(os.path.join(self.cacheDir, "checkpoints", self.modelName, "saved_model"))
+
+        print("Model " + self.modelName + " loaded successfully")
+    
+    def createBoundigBox(self, image, threshold = 0.5):
+        inputTensor = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2RGB)
+        inputTensor = tf.convert_to_tensor(inputTensor, dtype=tf.uint8)
+        inputTensor = inputTensor[tf.newaxis, ...]
+
+        # ## perf testing, next 1 line
+        # detStartTime = time.time()
+        detections = self.model(inputTensor)
+        # ## perf testing, next 3 line3
+        # detEndTime = time.time()
+        # detFPS = 1/((detEndTime + 0.000000000001) - detStartTime)
+        # print("detFPS = ", detFPS)
+
+        bboxs = detections['detection_boxes'][0].numpy()
+        classIndexes = detections['detection_classes'][0].numpy().astype(np.int32)
+        classScores = detections['detection_scores'][0].numpy()
+
+        imH, imW, imC = image.shape
+
+        bboxIdx = tf.image.non_max_suppression(bboxs, classScores, max_output_size=50,
+                                               iou_threshold=threshold, score_threshold=threshold)
+        
+        ## for testing
+        # print(bboxIdx)
+
+        if len(bboxIdx) != 0:
+            for i in bboxIdx:
+                bbox = tuple(bboxs[i].tolist())
+                classConfidence = round(100*classScores[i])
+                classIndex = classIndexes[i]
+
+                classLabelText = self.classesList[classIndex]
+                classColor = self.colorList[classIndex]
+
+                displayText = '{}: {}%'.format(classLabelText, classConfidence)
+
+                ymin, xmin, ymax, xmax = bbox
+
+                xmin, xmax, ymin, ymax = (xmin * imW, xmax * imW, ymin * imH, ymax * imH)
+                xmin, xmax, ymin, ymax = int(xmin), int(xmax), int(ymin), int(ymax)
+
+                cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color=classColor, thickness=1)
+                cv2.putText(image, displayText, (xmin, ymin -10 ), cv2.FONT_HERSHEY_PLAIN, 1, classColor, 2)
+
+        return image
+
+    
+    
+    def predictImage(self, imagePath, threshold = 0.5):
+        image = cv2.imread(imagePath)
+
+        bboxImage = self.createBoundigBox(image, threshold)
+
+        cv2.imwrite(self.modelName + ".jpg", bboxImage)
+        cv2.imshow("Result", bboxImage)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    
+    def predictVideo(self, videoPath, threshold = 0.5):
+        cap = cv2.VideoCapture(videoPath)
+
+        if (cap.isOpened() == False):
+            print("Error opening file...")
+            return
+        
+        (success, image) = cap.read()
+
+        startTime = 0
+        
+        while success:
+            currentTime = time.time()
+
+            fps = 1/(currentTime - startTime)
+            startTime = currentTime
+
+            bboxImage = self.createBoundigBox(image, threshold)
+
+            cv2.putText(bboxImage, "FPS: " + str(int(fps)), (20, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0,255,0),2)
+            cv2.imshow("Result", bboxImage)
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                break
+
+            (success, image) = cap.read()
+        cv2.destroyAllWindows()
+    
+
+    def capture_window(self):
+        # game in 1024 x 768 windowed mode
+        image =  np.array(ImageGrab.grab(bbox=(0,0,1024,768)))
+        return image
+
+
+    def predictImgCap(self, threshold = 0.5):
+        image = self.capture_window()
+
+        startTime = 0
+        
+        while True:
+            currentTime = time.time()
+
+            fps = 1/(currentTime - startTime)
+            startTime = currentTime
+
+            bboxImage = self.createBoundigBox(image, threshold)
+
+            cv2.putText(bboxImage, "FPS: " + str(int(fps)), (20, 70), cv2.FONT_HERSHEY_PLAIN, 2, (0,255,0),2)
+            cv2.imshow("Result", bboxImage)
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                break
+
+            image = self.capture_window()
+        cv2.destroyAllWindows()
+    
+
